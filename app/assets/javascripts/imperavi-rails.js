f23e7d4940412711a3370921b849721484cd3309
PitOn <garik.piton@gmail.com>
2012-06-15 16:29:27 +0400

    Update new_release

--- app/assets/javascripts/imperavi-rails.js
+++ app/assets/javascripts/imperavi-rails.js
@@ -42,8 +42,8 @@ var RTOOLBAR = {};
 			fileUpload:     false, // url
 			handler:        false, // false or url
 			autosave:       false, // false or url
-			imageUpload:    'forum/post_images', // url
-			imageGetJson:   'forum/post_images.json', // url (ex. /folder/images.json ) or false
+			imageUpload:    '/forum/post_images', // url
+			imageGetJson:   '/forum/post_images.json', // url (ex. /folder/images.json ) or false
 
 			load: true,
 		
